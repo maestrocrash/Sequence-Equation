3468781bd9f96feeabdcbb7ae16cf77fa7a8216d
maestrocrash <maestrocrash@gmail.com>
2019-10-09 15:41:39 +0300

    Initial Commit

---
+++ Sequence Equation/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Sequence Equation
+//
+//  Created by Shakhvorostov on 09.10.2019.
+//  Copyright © 2019 Shakhvorostov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
