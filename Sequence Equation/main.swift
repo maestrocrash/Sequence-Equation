23761f1f4aaeaf385914a81f219735dff0c0cfc2
maestrocrash <maestrocrash@gmail.com>
2019-10-10 11:17:02 +0300

    Working

--- Sequence Equation/main.swift
+++ Sequence Equation/main.swift
@@ -19,17 +19,15 @@ func permutationEquation(p: [Int]) -> [Int] {
         dic[n+1] = p[n]
         n += 1
     }
-    
+    var x: Int = 1
+    var k: Int = 1
     while m != p.count {
+        var h: Int = 1
         
-        if dic[m+1] == dic[dic[m+1]!]{
-            newP.append(dic[m+1]!)
-        }
-        
+        newP[m] = h
+        x += 1
         m += 1
     }
-    
-    print(dic)
     
     return newP
 }
